a4f5ddd2e873c0c74e7d3c5b9a46a07b1bbbfe41
Jaime Kvaternik <jaime.kvaternik17@gmail.com>
2022-11-02 00:19:34 -0400

    remove controller functionality from frontend

--- frontend/src/components/Town/TownMap.tsx
+++ frontend/src/components/Town/TownMap.tsx
@@ -5,6 +5,7 @@ import useTownController from '../../hooks/useTownController';
 import SocialSidebar from '../SocialSidebar/SocialSidebar';
 import NewConversationModal from './interactables/NewCoversationModal';
 import TownGameScene from './TownGameScene';
+import CrosswordGameModal from '../Crossword/CrosswordGameModal';
 
 export default function TownMap(): JSX.Element {
   const coveyTownController = useTownController();
@@ -48,6 +49,7 @@ export default function TownMap(): JSX.Element {
   return (
     <div id='app-container'>
       <NewConversationModal />
+      <CrosswordGameModal />
       <div id='map-container' />
       <div id='social-container'>
         <SocialSidebar />
--- frontend/src/components/Town/interactables/Crossword/CrosswordGrid/CrosswordCell.test.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { act } from 'react-dom/test-utils';
-
-describe('CrosswordCell', () => {});
