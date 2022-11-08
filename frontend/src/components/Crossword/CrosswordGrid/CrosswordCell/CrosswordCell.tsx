17c975fbfbbed212a614b8444a1622ea1437b270
Jaime Kvaternik <34807814+jkvaternik@users.noreply.github.com>
2022-11-08 14:08:56 -0500

    Frontend/add xw frontend (#6)
    * add what I have now
    
    * add frontend, sans tests
    
    * whoops
    
    * remove controller functionality from frontend
    
    * remove controller functionality from view, add modal, connect to area
    
    * clean up
    
    * fix lint
    
    * remove commented out code
    
    * fix lint
    
    * add useState back, add TODO
    
    * clean up, fix lint

---
+++ frontend/src/components/Crossword/CrosswordGrid/CrosswordCell/CrosswordCell.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './CrosswordCell.css';
+
+function CrosswordCell(props: {
+  cellID: string;
+  isRebus: boolean;
+  isSelected: boolean;
+  value: string;
+}): JSX.Element {
+  if (props.value === '.') {
+    return <td className='filled'></td>;
+  }
+  if (props.isSelected) {
+    return (
+      <td className='selected'>
+        <input value={props.value} />
+      </td>
+    );
+  }
+  return (
+    <td>
+      <input value={props.value} />
+    </td>
+  );
+}
+
+export default CrosswordCell;
