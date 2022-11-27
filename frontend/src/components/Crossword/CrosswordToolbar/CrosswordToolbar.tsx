6d7f7dc808f9f8d22c809b7707a3fd8a3b0d5003
Skarner <skarner@Skarners-MacBook-Pro.local>
2022-11-26 20:22:53 -0500

    quick fix

--- frontend/src/components/Crossword/CrosswordToolbar/CrosswordToolbar.tsx
+++ frontend/src/components/Crossword/CrosswordToolbar/CrosswordToolbar.tsx
@@ -11,11 +11,8 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import LeaderboardModal from './LeaderboardModal';
-import Timer from './Timer';
-import CrosswordPuzzleAreaController, {
-  useCrosswordPuzzleAreaOccupants,
-} from '../../../classes/CrosswordPuzzleAreaController';
-import OccupantsDisplay from './OccupantsDisplay';
+import CrosswordPuzzleAreaController from '../../../classes/CrosswordPuzzleAreaController';
+
 import { PlayerLocation } from '../../../../../shared/types/CoveyTownSocket';
 import PlayerController from '../../../classes/PlayerController';
 import { nanoid } from 'nanoid';
@@ -23,21 +20,6 @@ import { nanoid } from 'nanoid';
 const CELL_WIDTH = 28;
 
 function CrosswordToolbar({ controller }: { controller: CrosswordPuzzleAreaController }) {
-  // TODO: uncomment when multiplayer is fixed
-  // const occupants = useCrosswordPuzzleAreaOccupants(controller);
-  const playerLocation: PlayerLocation = {
-    moving: false,
-    x: 0,
-    y: 0,
-    rotation: 'front',
-  };
-  const occupants = [
-    new PlayerController(nanoid(), nanoid(), playerLocation),
-    new PlayerController(nanoid(), nanoid(), playerLocation),
-    new PlayerController(nanoid(), nanoid(), playerLocation),
-    new PlayerController(nanoid(), nanoid(), playerLocation),
-    new PlayerController(nanoid(), nanoid(), playerLocation),
-  ];
   const { isOpen, onOpen, onClose } = useDisclosure();
   const timerWidth = controller.puzzle?.grid.length
     ? controller.puzzle?.grid.length * CELL_WIDTH
@@ -45,7 +27,7 @@ function CrosswordToolbar({ controller }: { controller: CrosswordPuzzleAreaContr
   return (
     <Flex gap={'2'} paddingBottom={'12px'}>
       <Box p='2' bg='gray.200' width={timerWidth} textAlign='center'>
-        <Timer />
+        00:00
       </Box>
       <Menu>
         <MenuButton p='4' as={Button} colorScheme='gray'>
@@ -61,8 +43,6 @@ function CrosswordToolbar({ controller }: { controller: CrosswordPuzzleAreaContr
       <Button p='4' colorScheme='gray'>
         Rebus
       </Button>
-
-      <OccupantsDisplay players={occupants} />
 
       <Spacer></Spacer>
       <Button p='4' colorScheme='blue' size='md' onClick={onOpen}>
