9773b2156e63464a4a8f4101f874f6129ed4cfe7
Frank Isaacson <isaacson.f@northeastern.edu>
2022-11-29 23:53:34 -0500

    added comments

--- townService/src/db/DatabaseErrors/NotFoundError.ts
+++ townService/src/db/DatabaseErrors/NotFoundError.ts
@@ -1,3 +1,6 @@
+/**
+ * Represents error that occurs when the database returns Null and a value is needed to be returned
+ */
 export default class NotFoundError extends Error {
   constructor(message: string) {
     super(message);
--- townService/src/db/DatabaseErrors/UndefinedError.ts
+++ townService/src/db/DatabaseErrors/UndefinedError.ts
@@ -1,3 +1,6 @@
+/**
+ * Represents error that occurs when the database returns Undefined and a value is needed to be returned
+ */
 export default class UndefinedError extends Error {
   constructor(message: string) {
     super(message);
